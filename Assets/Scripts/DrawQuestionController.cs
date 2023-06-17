f66078921b0a85613aef8f9314a341d63502ebfc
UltraMarlin <68075515+UltraMarlin@users.noreply.github.com>
2023-06-17 20:25:34 +0200

    Add working draw Question example

--- Assets/Scripts/DrawQuestionController.cs
+++ Assets/Scripts/DrawQuestionController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class DrawQuestionController : MonoBehaviour, IQuestionController
 {
     [SerializeField] private GameObject videoContainer;
     [SerializeField] private GameObject stretchVideoPrefab;
     [SerializeField] private TextMeshProUGUI solutionText;
+    [SerializeField] private TextMeshProUGUI drawerTitle;
+    [SerializeField] private TextMeshProUGUI drawerText;
 
+    private VideoPlayer drawVideoPlayerComponent;
     public DrawQuestion drawQuestionData;
 
     public void SetData(IQuestion questionData)
@@ -19,21 +23,38 @@ public class DrawQuestionController : MonoBehaviour, IQuestionController
 
     public void StartQuestion()
     {
+        drawerTitle.enabled = false;
         if (drawQuestionData == null) return;
+        GameObject drawVideo = Instantiate(stretchVideoPrefab, videoContainer.transform);
+        drawVideoPlayerComponent = drawVideo.GetComponent<VideoPlayer>();
+        drawVideoPlayerComponent.clip = drawQuestionData.videoClip;
     }
 
     public void NextQuestionStep()
     {
-        return;
+        if (drawVideoPlayerComponent == null) return;
+
+        if (drawVideoPlayerComponent.isPlaying)
+        {
+            drawVideoPlayerComponent.Pause();
+        } else
+        {
+            drawVideoPlayerComponent.Play();
+        }
     }
 
     public void ShowSolution()
     {
-        return;
+        drawVideoPlayerComponent.Pause();
+        drawVideoPlayerComponent.frame = (long)drawVideoPlayerComponent.frameCount - 1;
+        solutionText.text = drawQuestionData.pokemonName;
+        drawerTitle.enabled = true;
+        drawerText.text = drawQuestionData.drawerName;
     }
 
     public void ResetDisplay()
     {
-        return;
+        drawVideoPlayerComponent.Pause();
+        drawVideoPlayerComponent.Play();
     }
 }
--- Assets/Scripts/ScriptableObjects/DrawQuestion.cs
+++ Assets/Scripts/ScriptableObjects/DrawQuestion.cs
@@ -1,18 +1,21 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Video;
 
 [CreateAssetMenu(fileName = "DrawQuestion", menuName = "QuestionTypes/Draw", order = 5)]
 public class DrawQuestion : IQuestion
 {
     public string pokemonName;
-    public Sprite sprite;
+    public string drawerName;
+    public VideoClip videoClip;
 
     public void OnValidate()
     {
-        if (!QuizUtils.validateQuestionObjects) return;
-        if (sprite != null)
+        //if (!QuizUtils.validateQuestionObjects) return;
+        if (videoClip != null)
         {
-            pokemonName = sprite.name.Split("_")[1];
+            pokemonName = videoClip.name.Split('_')[0];
+            drawerName = videoClip.name.Split('_')[1];
         }
 
         if (pokemonName.Length > 0)
